fe0b5bca992f139860bfd1e28177cd16a02cfcd8
Rahul Ballal <rahulballal@gmail.com>
2023-06-06 22:08:41 +1000

    a bit of cleanup

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,22 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-const BASE_URL = "/";
 test("list page is operational", async ({ page }) => {
-  await page.goto(BASE_URL);
+  await page.goto("/");
 
   await expect(page.getByText("Cats World")).toBeDefined();
 });
 
 test("individual cat can be retreived", async ({ page }) => {
-  const firstCatPath = "cat/464e88b1-dc61-4804-b74b-b49960d413eb";
-  await page.goto(`${BASE_URL}${firstCatPath}`);
+  const firstCatPath = "/cat/464e88b1-dc61-4804-b74b-b49960d413eb";
+  await page.goto(`${firstCatPath}`);
 
   await expect(page.getByText("Abyssinian")).toBeDefined();
 });
 
 test("can navigate to the new cat form", async ({ page }) => {
-  await page.goto(BASE_URL);
+  await page.goto("/");
   await page.getByText("Add Cat").click();
 
+  await expect(page.url().search("new-cat")).toBeTruthy();
   await expect(page.getByText("Submit")).toBeVisible();
 });
