f273c74a3578d19ec1143692a295a66897b7cfbd
Rahul Ballal <rahulballal@gmail.com>
2023-06-13 21:55:07 +1000

    adds better testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 // @ts-nocheck
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     test: {
         globals: true,
-        environment: 'jsdom',
-        setupFiles: ['testSetup.ts']
-    }
-})
+        environment: "jsdom",
+        setupFiles: ["testSetup.ts"],
+        exclude: [...configDefaults.exclude, "tests/*"],
+    },
+});
